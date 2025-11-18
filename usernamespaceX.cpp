93cc1d4fc009bae9d5b46d8b3e7f701e56966918
nikhilsuryavanshi0005 <nikhilsuryavanshi0005@gmail.com>
2025-11-19 00:33:17 +0530

    Add files via upload

---
+++ usernamespaceX.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+namespace PPA
+{
+    int i = 11;
+}
+
+namespace LB
+{
+    int j = 21;
+}
+using namespace LB;
+
+int main()
+{
+    cout<<PPA::i<<"\n";
+
+    cout<<j<<"\n";
+
+    return 0;
+
+}
